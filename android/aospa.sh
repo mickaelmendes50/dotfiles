0d8487e5ba57d3548240baa6088a429dece94b0d
Mesquita <mesquita@aospa.co>
2023-04-21 19:35:28 +0000

    android: use depth on repo init
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- android/aospa.sh
+++ android/aospa.sh
@@ -3,5 +3,5 @@ mkdir ~/aospa
 cd ~/aospa
 
 # Init aospa source and sync it with depth
-repo init -u https://github.com/AOSPA/manifest -b topaz
-repo sync --no-clone-bundle --current-branch --no-tags -j16 --depth=1
+repo init -u https://github.com/AOSPA/manifest -b topaz --depth=1
+repo sync --no-clone-bundle --current-branch --no-tags -j16
