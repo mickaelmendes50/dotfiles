dea196a9ac2ff06375d001369866b54a4340bb14
Mesquita <mesquita@aospa.co>
2023-04-21 19:17:14 +0000

    linux: add script for arch gnome post-install
    Signed-off-by: Mesquita <mesquita@aospa.co>

---
+++ linux/arch-gnome.sh
@@ -0,0 +1,23 @@
+#
+# Script to setup ArchLinux on Gnome UI
+#
+sudo pacman -Syu
+
+# Useful Packages
+sudo pacman -S gedit byobu neofetch gufw fwupd ntfs-3g flatpak gnome-software-packagekit-plugin ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer git base-devel
+
+# Install Yay
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+yay -Y --gendb
+
+cd ..
+
+rm -rf yay
+
+# Enable bluetooth service
+sudo systemctl enable bluetooth.service 
+sudo systemctl start bluetooth.service --now
+
+# Install my apps
+yay -S google-chrome vscode sublime-text-4 telegram-desktop slack-desktop whatsdesk-bin
